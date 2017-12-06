81dbc325bc1c7677916067f4247138450e2f5bfb
Avery Broderick <avery.c.broderick@gmail.com>
2017-12-06 14:13:20 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,12 +20,12 @@ WIN_COMBINATIONS = [
 def won?(board)
   # binding.pry
   WIN_COMBINATIONS.each do |combo|
-    puts "#{combo}"
     if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X"
       return combo
     elsif board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
       return combo
     else
       return false
+    end
   end
 end
